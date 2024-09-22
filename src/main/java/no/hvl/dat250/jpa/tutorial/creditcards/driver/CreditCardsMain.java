1f13b5398a1f5be90a4798c44f837f81c7f7ce00
toretveita <toretveita@gmail.com>
2024-09-22 19:45:05 +0200

    Changes to main

--- src/main/java/no/hvl/dat250/jpa/tutorial/creditcards/driver/CreditCardsMain.java
+++ src/main/java/no/hvl/dat250/jpa/tutorial/creditcards/driver/CreditCardsMain.java
@@ -20,28 +20,22 @@ public class CreditCardsMain {
   }
 
   private static void createObjects(EntityManager em) {
-    // Create the customer
     Customer customer = new Customer();
     customer.setName("Max Mustermann");
 
-    // Create the address
     Address address = new Address();
     address.setStreet("Inndalsveien");
     address.setNumber(28);
 
-    // Associate the address with the customer
     customer.getAddresses().add(address);
 
-    // Create the bank
     Bank bank = new Bank();
     bank.setName("Pengebank");
 
-    // Create the first pincode
     Pincode pincode1 = new Pincode();
     pincode1.setCode("123");
     pincode1.setCount(1);
 
-    // Create the first credit card
     CreditCard card1 = new CreditCard();
     card1.setNumber(12345);
     card1.setBalance(-5000);
@@ -49,7 +43,6 @@ public class CreditCardsMain {
     card1.setOwningBank(bank);
     card1.setPincode(pincode1);
 
-    // Create the second credit card
     CreditCard card2 = new CreditCard();
     card2.setNumber(123);
     card2.setBalance(1);
@@ -57,11 +50,10 @@ public class CreditCardsMain {
     card2.setOwningBank(bank);
     card2.setPincode(pincode1); // Same pincode as card1
 
-    // Associate the credit cards with the customer
     customer.getCreditCards().add(card1);
     customer.getCreditCards().add(card2);
 
-    // Persist all entities
+    // Persist
     em.persist(customer);
     em.persist(address);
     em.persist(bank);
@@ -69,5 +61,4 @@ public class CreditCardsMain {
     em.persist(card1);
     em.persist(card2);
   }
-
 }
